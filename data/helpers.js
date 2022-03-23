a1a89513080e2c6f03d0920a3a4369a4bb898bda
Isaiah <isaiah.iu@hotmail.com>
2022-03-23 18:01:23 +0000

    refactored express_server, moved functions and data objects into separate files. Fixed error with registration where you could log into an existing email. Register works as intended now

---
+++ data/helpers.js
@@ -0,0 +1,31 @@
+function createUser(email, password, usersObj) {
+	if (!email || !password || password.length < 6) {
+		return { error: "Invalid Fields", data: null };
+	}
+	for (const user in usersObj) {
+		if (usersObj[user].email === email) {
+			return { error: "Email already in use!", data: null };
+		}
+	}
+	const user = {
+		id: generateRandomString(),
+		email,
+		password,
+	};
+	usersObj[user.id] = user;
+	return { error: null, data: user };
+}
+
+function generateRandomString() {
+	let result = "";
+	while (result.length < 6) {
+		let char = Math.floor(Math.random() * (123 - 48) + 48);
+		if ((char >= 58 && char <= 64) || (char >= 91 && char <= 96)) {
+			continue;
+		}
+		result += String.fromCharCode(char);
+	}
+	return result;
+}
+
+module.exports = { createUser, generateRandomString };
--- express_server.js
+++ express_server.js
@@ -1,56 +1,17 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const req = require("express/lib/request");
 const app = express();
 const PORT = 3000; // default port 3000
+const req = require("express/lib/request");
+const res = require("express/lib/response");
+const bodyParser = require("body-parser");
+const { createUser, generateRandomString } = require("./data/helpers");
+const { users, urlDatabase } = require("./data/database");
 
 app.set("view engine", "ejs");
-
-const bodyParser = require("body-parser");
-const res = require("express/lib/response");
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-function userSearch(reqObj, usersObj) {
-	for (let user in usersObj) {
-		console.log(`user is: `, user);
-		console.log(`reqObj.email is: `, reqObj.email);
-		console.log(`user email is : `, usersObj[user].email);
-		console.log(``);
-		if (reqObj.email === usersObj[user].email) {
-			return true;
-		}
-		return false;
-	}
-}
-
-function generateRandomString() {
-	let result = "";
-	while (result.length < 6) {
-		let char = Math.floor(Math.random() * (123 - 48) + 48);
-		if ((char >= 58 && char <= 64) || (char >= 91 && char <= 96)) {
-			continue;
-		}
-		result += String.fromCharCode(char);
-	}
-	return result;
-}
-const urlDatabase = {
-	b2xVn2: "http://www.lighthouselabs.ca",
-	"9sm5xK": "http://www.google.com",
-};
-const users = {
-	userRandomID: {
-		id: "userRandomID",
-		email: "user@example.com",
-		password: "purple-monkey-dinosaur",
-	},
-	user2RandomID: {
-		id: "user2RandomID",
-		email: "user2@example.com",
-		password: "dishwasher-funk",
-	},
-};
 app.get("/", (req, res) => {
 	res.send("Hello!");
 });
@@ -73,9 +34,6 @@ app.get("/urls", (req, res) => {
 		user: users[loginID],
 		urls: urlDatabase,
 	};
-	// console.log("user key is: ", loginID);
-	// console.log("users object is: ", users);
-	// console.log("templateVars is : ", templateVars);
 	res.render("urls_index", templateVars);
 });
 
@@ -105,21 +63,12 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-	if (!req.body.email || !req.body.password || req.body.password.length < 6) {
-		return res.status(400).send("Invalid Fields");
+	const { email, password } = req.body;
+	const { error, data } = createUser(email, password, users);
+	if (error) {
+		return res.status(400).send(error);
 	}
-	if (userSearch(req.body, users)) {
-		return res.status(400).send("Email already in use!");
-	}
-	const newId = generateRandomString();
-	const user = {
-		id: newId,
-		email: req.body.email,
-		password: req.body.password,
-	};
-	users[newId] = user;
-	res.cookie("user_id", newId);
-	console.log(users);
+	res.cookie("user_id", data.id);
 	res.redirect("/urls");
 });
 
@@ -133,9 +82,6 @@ app.post("/login", (req, res) => {
 	const templateVars = {
 		user: users[req.cookies["user_id"]],
 	};
-	// console.log("user key is: ", loginID);
-	// console.log("users object is: ", users);
-	// console.log("templateVars is : ", templateVars);
 	res.redirect("/urls");
 });
 
