f2eb69c6e694a23476030fccd0d581c6e08ba4d3
Isaiah <isaiah.iu@hotmail.com>
2022-03-24 00:02:41 +0000

    cleaned up html files

--- express_server.js
+++ express_server.js
@@ -1,9 +1,5 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const app = express();
-const PORT = 3000; // default port 3000
-const req = require("express/lib/request");
-const res = require("express/lib/response");
 const bodyParser = require("body-parser");
 const {
 	createUser,
@@ -12,24 +8,40 @@ const {
 } = require("./data/helpers");
 const { users, urlDatabase } = require("./data/database");
 
+const PORT = 3000; 
+
+const app = express();
+
+app.set("etag", false);
 app.set("view engine", "ejs");
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-	res.send("Hello!");
-});
-
-app.get("/hello", (req, res) => {
-	res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
 
 app.get("/register", (req, res) => {
 	res.render("register");
 });
 
+app.post("/register", (req, res) => {
+	const { email, password } = req.body;
+	const { error, data } = createUser(email, password, users);
+	if (error) {
+		return res.status(400).send(error);
+	}
+	res.cookie("user_id", data.id);
+	res.redirect("/urls");
+});
+
 app.get("/login", (req, res) => {
 	res.render("login");
+});
+
+app.post("/login", (req, res) => {
+	const { error, data } = authenticateUser(req.body, users);
+	if (error) {
+		return res.status(403).send(error);
+	}
+	res.cookie("user_id", data);
+	res.redirect("/urls");
 });
 
 app.get("/urls", (req, res) => {
@@ -39,6 +51,12 @@ app.get("/urls", (req, res) => {
 		urls: urlDatabase,
 	};
 	res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+	const short = generateRandomString();
+	urlDatabase[short] = req.body.longURL;
+	res.redirect(`/urls/${short}`);
 });
 
 app.get("/urls.json", (req, res) => {
@@ -61,41 +79,6 @@ app.get("/urls/:shortURL", (req, res) => {
 	res.render("urls_show", templateVars);
 });
 
-app.get("/u/:shortURL", (req, res) => {
-	const longURL = urlDatabase[req.params.shortURL];
-	res.redirect(longURL);
-});
-
-app.post("/register", (req, res) => {
-	const { email, password } = req.body;
-	const { error, data } = createUser(email, password, users);
-	if (error) {
-		return res.status(400).send(error);
-	}
-	res.cookie("user_id", data.id);
-	res.redirect("/urls");
-});
-
-app.post("/urls", (req, res) => {
-	const short = generateRandomString();
-	urlDatabase[short] = req.body.longURL;
-	res.redirect(`/urls/${short}`);
-});
-
-app.post("/login", (req, res) => {
-	const { error, data } = authenticateUser(req.body, users);
-	if (error) {
-		return res.status(403).send(error);
-	}
-	res.cookie("user_id", data);
-	res.redirect("/urls");
-});
-
-app.post("/logout", (req, res) => {
-	res.clearCookie("user_id");
-	res.redirect("/urls");
-});
-
 app.post("/urls/:shortURL", (req, res) => {
 	const id = req.params.shortURL;
 	urlDatabase[id] = req.body.newURL;
@@ -106,6 +89,17 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 	delete urlDatabase[req.params.shortURL];
 	res.redirect("/urls");
 });
+
+app.get("/u/:shortURL", (req, res) => {
+	const longURL = urlDatabase[req.params.shortURL];
+	res.redirect(longURL);
+});
+
+app.post("/logout", (req, res) => {
+	res.clearCookie("user_id");
+	res.redirect("/urls");
+});
+
 
 app.listen(PORT, () => {
 	console.log(`TinyApp listening on port ${PORT}!`);
